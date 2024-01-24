425604d3481a19bb1b74d786757de40280465ab0
Basant-Adel <basantadel99@gmail.com>
2024-01-24 23:34:00 +0000

    Implementing Web Cache & Redis 4 :D

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,24 +5,29 @@ import requests
 from functools import wraps
 from typing import Callable
 
-
 redis_store = redis.Redis()
-""" Redis Store """
 
 
 def data_cacher(method: Callable) -> Callable:
     """ Data Cacher """
     @wraps(method)
-    def invoker(url) -> str:
+    def invoker(url: str) -> str:
         """ Invoker """
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
+        count_key = f'count:{url}'
+        result_key = f'result:{url}'
+
+        redis_store.incr(count_key)
+
+        result = redis_store.get(result_key)
         if result:
             return result.decode('utf-8')
+
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+
+        redis_store.setex(result_key, 10, result)
+
         return result
+
     return invoker
 
 
@@ -30,3 +35,16 @@ def data_cacher(method: Callable) -> Callable:
 def get_page(url: str) -> str:
     """ Get Page """
     return requests.get(url).text
+
+
+if __name__ == "__main__":
+    slow_url = (
+        "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.google.com"
+    )
+
+    for _ in range(5):
+        content = get_page(slow_url)
+        print(content)
+
+    access_count = redis_store.get(f'count:{slow_url}')
+    print(f'Total Access Count for {slow_url}: {access_count.decode("utf-8")} times')
